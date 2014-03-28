e8daaac9a7c0ec7c8f5320e1fb56b0a9c8cf514d
Henrik Almér <henrik.almer@gmail.com>
2014-03-28 22:44:38 +0100

    Refine SVM configuration

--- svm.py
+++ svm.py
@@ -2,21 +2,45 @@ import json
 import numpy as np
 import pylab as pl
 from sklearn import svm
-from data import load_features
+from data import load_features, load_mfccs
 
 
+# Load and divide data
+
+##songs, prop_dict = load_mfccs()
 songs, prop_dict = load_features()
+training_set = dict([e for e in prop_dict.items()[0:30]])
+testing_set = dict([e for e in prop_dict.items()[30:]])
+
+
+# Train SVM
 
 fo = open('data/drums.genres.json', 'r')
 genres = json.loads(fo.read())
 cmap = {'pop': 0, 'rock': 1, 'reggae': 2, 'jazz': 3, 'classical': 4}
-classes = [cmap[genres[k]] for k in prop_dict.keys()  if '-2' not in k]
+classes = [cmap[genres[k]] for k in training_set.keys()]
 fo.close()
 
-X = [p for k, p in prop_dict.items() if '-2' not in k]
-Y = classes
-clf = svm.SVC()
-clf.fit(X, Y)
+X = np.array([p for k, p in training_set.items()])
+Y = np.array(classes)
+C = 1.0  # SVM regularization parameter
+rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(X, Y)
 
-print clf.predict(prop_dict['wav/drums-short/abba-drums-2.wav'])
 
+# Test SVM
+
+validate = np.array([p for k, p in testing_set.items()])
+
+hits = 0.0
+misses = 0.0
+for s, p in [(s, p) for (s, p) in testing_set.items()]:
+    s_name = s.split('/')[2].split('.')[0]
+    prediction = rbf_svc.predict(prop_dict[s])
+    answer = cmap[genres[s]]
+    if answer == prediction:
+        hits += 1
+    else:
+        misses += 1
+    print s_name, ': ', answer, prediction, answer == prediction
+
+print 'Success rate: ', hits/(hits+misses)
