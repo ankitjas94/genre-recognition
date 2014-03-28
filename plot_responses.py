fed9b4511b84302b22ecec4e5e8a30c124987366
Henrik Almér <henrik.almer@gmail.com>
2014-03-28 17:53:54 +0100

    Cleanup

--- plot_responses.py
+++ plot_responses.py
@@ -4,11 +4,15 @@ from data import load_responses
 
 songs, responses = load_responses()
 
-fig1 = pl.figure(1)
+# Print table of median values
+median_vectors = dict([(s, np.median(r, axis=0)) for s, r in responses.items()])
+for s, v in median_vectors.items():
+    print "%s\t%s" % (s.split('/')[2].split('.')[0], '\t'.join(str(val) for val in v))
 
 # Plot mean values
-mean_vectors = [(s, np.median(r, axis=0)) for s, r in responses.items()]
-track = dict(mean_vectors)['wav/drums-short/wham-drums-2.wav']
+fig1 = pl.figure(1)
+
+track = median_vectors['wav/drums-short/wham-drums-2.wav']
 
 plot1 = fig1.add_subplot(121)
 plot1.plot(track, 'go')
@@ -17,7 +21,7 @@ labels = ('Pop', 'Rock', 'Reggae', 'Jazz', 'Klassiskt')
 pl.xticks(range(0,5), labels, rotation=20)
 pl.ylim([0,100])
 pl.xlim([-.5,5])
-pl.title('Wham - Mean values')
+pl.title('Wham - Medel')
 
 # Plot boxplot and whiskers
 track = responses['wav/drums-short/wham-drums-2.wav']
@@ -35,5 +39,5 @@ labels = ('Pop', 'Rock', 'Reggae', 'Jazz', 'Klassiskt')
 pl.xticks(range(1,6), labels, rotation=20)
 pl.title('Wham - Boxplot')
 
-# Show
+# Show plots
 pl.show()
